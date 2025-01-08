b6b3d5cd5c27df9663fc7fa6119f3df2d8b9739e
Bennett <vschun2024@gmail.com>
2025-01-08 16:57:54 +0800

    修改後台Layout.cshtml/Login.cshtml/Register.cshtml

---
+++ Spotix.MVC/Controllers/PlaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Spotix.MVC.Controllers
+{
+    public class PlaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Spotix.Utilities/Models/ViewModels/PlaceVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotix.Utilities.Models.ViewModels
+{
+    public class PlaceVM
+    {
+        [Display(Name = "場地名稱")]
+        [Required(ErrorMessage = "{0} 為必填")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Display(Name = "顯示順序")]
+        [Required(ErrorMessage = "{0} 為必填")]
+        public int DisplayOrder { get; set; }
+
+        [Display(Name = "是否可使用")]
+        [Required(ErrorMessage = "{0} 為必填")]
+        public bool Enabled { get; set; }
+
+    }
+}
